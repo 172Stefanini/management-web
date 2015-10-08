ee981369f4719bd81992c67e18c950958ef1c889
fpachecos <fpacheco@192.168.1.15>
2015-10-07 21:43:00 -0300

    - Lista de Lançamentos Pendentes

--- src/main/java/com/stefanini/bob/management/domain/Person.java
+++ src/main/java/com/stefanini/bob/management/domain/Person.java
@@ -1,10 +1,16 @@
 package com.stefanini.bob.management.domain;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import javax.persistence.Column;
 
 @RooJavaBean
 @RooToString
@@ -28,4 +34,42 @@ public class Person {
      */
     @Column(unique = true)
     private String accessUserName;
+
+	public static List<Person> findAllPendingPeople(String sortFieldName, String sortOrder) {
+		String jpaQuery = "SELECT p FROM Person p";
+        jpaQuery += " where 16 > (";
+        jpaQuery += "    select sum(t.workHours) from TimeSheet t where t.person = p.id and (occurrenceDate = :now or occurrenceDate = :yesterday)";
+        jpaQuery += " ) or not exists (";
+        jpaQuery += "    select t from TimeSheet t where t.person = p.id and (occurrenceDate = :now or occurrenceDate = :yesterday)";
+        jpaQuery += " )";
+        if (fieldNames4OrderClauseFilter.contains(sortFieldName)) {
+            jpaQuery = jpaQuery + " ORDER BY " + sortFieldName;
+            if ("ASC".equalsIgnoreCase(sortOrder) || "DESC".equalsIgnoreCase(sortOrder)) {
+                jpaQuery = jpaQuery + " " + sortOrder;
+            }
+        }
+        return entityManager().createQuery(jpaQuery, Person.class)
+        		.setParameter("now", new Date(), TemporalType.DATE)
+        		.setParameter("yesterday", DateUtils.addDays(new Date(), -1), TemporalType.DATE)
+        		.getResultList();
+    }
+
+	public static List<Person> findPendingPersonEntries(int firstResult, int maxResults, String sortFieldName, String sortOrder) {
+		String jpaQuery = "SELECT p FROM Person p";
+        jpaQuery += " where 16 > (";
+        jpaQuery += "    select sum(t.workHours) from TimeSheet t where t.person = p.id and (occurrenceDate = :now or occurrenceDate = :yesterday)";
+        jpaQuery += " ) or not exists (";
+        jpaQuery += "    select t from TimeSheet t where t.person = p.id and (occurrenceDate = :now or occurrenceDate = :yesterday)";
+        jpaQuery += " )";
+        if (fieldNames4OrderClauseFilter.contains(sortFieldName)) {
+            jpaQuery = jpaQuery + " ORDER BY " + sortFieldName;
+            if ("ASC".equalsIgnoreCase(sortOrder) || "DESC".equalsIgnoreCase(sortOrder)) {
+                jpaQuery = jpaQuery + " " + sortOrder;
+            }
+        }
+        return entityManager().createQuery(jpaQuery, Person.class)
+        		.setParameter("now", new Date(), TemporalType.DATE)
+        		.setParameter("yesterday", DateUtils.addDays(new Date(), -1), TemporalType.DATE)
+        		.setFirstResult(firstResult).setMaxResults(maxResults).getResultList();
+    }
 }
--- src/main/java/com/stefanini/bob/management/web/PersonController.java
+++ src/main/java/com/stefanini/bob/management/web/PersonController.java
@@ -1,11 +1,28 @@
 package com.stefanini.bob.management.web;
 import com.stefanini.bob.management.domain.Person;
+
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/people")
 @Controller
 @RooWebScaffold(path = "people", formBackingObject = Person.class)
 public class PersonController {
+	
+	@RequestMapping(params = "pendings", produces = "text/html")
+    public String pendings(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, @RequestParam(value = "sortFieldName", required = false) String sortFieldName, @RequestParam(value = "sortOrder", required = false) String sortOrder, Model uiModel) {
+        if (page != null || size != null) {
+            int sizeNo = size == null ? 10 : size.intValue();
+            final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
+            uiModel.addAttribute("people", Person.findPendingPersonEntries(firstResult, sizeNo, sortFieldName, sortOrder));
+            float nrOfPages = (float) personService.countAllPeople() / sizeNo;
+            uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
+        } else {
+            uiModel.addAttribute("people", Person.findAllPeople(sortFieldName, sortOrder));
+        }
+        return "people/listPendings";
+    }
 }
